e0f0e261bbdc6c33b809258b994db92519a29e63
Tom Riis <tomriis@toms-air.uconnect.utah.edu>
2018-11-15 11:43:04 -0700

    fieldname to and from params updated for element geometry and R_focus

--- FieldIIModels/dataviz_gui_utils/fieldname_from_params.m
+++ FieldIIModels/dataviz_gui_utils/fieldname_from_params.m
@@ -1,14 +1,20 @@
 function [fieldname]=fieldname_from_params(varargin)
 if isstruct(varargin{1})
     s = varargin{1};
-    n_elements=s.N; ROC=s.ROC; X=s.X; Y=s.Y; focus=s.F; P=s.P;
+    n_elements=s.N; ROC=s.ROC; X=s.X; Y=s.Y; focus=s.F; P=s.P; 
+    ElGeo = s.ElGeo; R_focus = s.Ro;
 else
     n_elements=varargin{1}; ROC=varargin{2}; X=varargin{3}; Y=varargin{4}; 
     focus=varargin{5}; P=varargin{6};
 end
 %returns txt field from params
+if ElGeo == 1
     runstring = strcat('N',num2str(n_elements),'ROC',num2str(ROC),'X',num2str(X),...
     'Y',num2str(Y),'F',num2str(focus),'P',num2str(P));
+else
+    runstring = strcat('ElGeo',num2str(ElGeo),'N',num2str(n_elements),'ROC',num2str(ROC),'X',num2str(X),...
+'Y',num2str(Y),'F',num2str(focus),'P',num2str(P),'Ro',num2str(R_focus));
+end
     fieldname = split(runstring,'.');
     fieldname = strcat(fieldname{:});
 end
--- FieldIIModels/dataviz_gui_utils/fieldname_to_param.m
+++ FieldIIModels/dataviz_gui_utils/fieldname_to_param.m
@@ -2,27 +2,40 @@ function [params] = fieldname_to_param(fieldname)
     % Take in fieldname string and return structure of params with
     % numerical values
     params = struct();
+    k_ElGeo = strfind(fieldname,'ElGeo');
     k_N = strfind(fieldname,'N');
     k_ROC = strfind(fieldname,'ROC');
     k_X = strfind(fieldname,'X');
     k_Y = strfind(fieldname,'Y');
     k_F = strfind(fieldname,'F');
     k_P = strfind(fieldname,'P');
+    k_Ro = strfind(fieldname,'Ro');
     params.N = str2double(fieldname(k_N+1:k_ROC-1));
     params.ROC = str2double(fieldname(k_ROC+3:k_X-1));
-    xname = fieldname(k_X+1:k_Y-1);
-    try 
-        params.X = str2double(strcat(xname(1),'.',xname(2:end)));
-    catch
-        params.X = str2double(xname(1));
-    end
+    params.X = get_param_decimal(fieldname, k_X+1,k_Y-1);
     
     params.Y = str2double(fieldname(k_Y+1:k_F-1));
     params.F = str2double(fieldname(k_F+1:k_P-1));
-    try
-        params.P = str2double(strcat(fieldname(k_P+1),'.',fieldname(k_P+2:end)));
-    catch
-        params.P=str2double(fieldname(k_P+1));
+    
+    % Handle the optional ElGeo and Ro
+    if isempty(k_ElGeo)
+        params.ElGeo = 1;
+    else
+        params.ElGeo = str2double(fieldname(k_ElGeo+5:k_N-1));
+    end
+    if isempty(k_Ro)
+        params.P = get_param_decimal(fieldname, k_P+1,length(fieldname));
+        params.Ro = inf;
+    else
+        params.P = get_param_decimal(fieldname, k_P+1,k_Ro-1);
+        params.Ro = str2double(fieldname(k_Ro+2:end));
     end
 end
 
+function [par] = get_param_decimal(fieldname, k_start, endpoint)
+    try
+        par = str2double(strcat(fieldname(k_start),'.',fieldname(k_start+1:endpoint)));
+    catch
+        par =str2double(fieldname(k_start));
+    end
+end
