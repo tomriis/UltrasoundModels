c7257b03f6ff428d9a7c6b46d0f01b47f76e4717
Tom Riis <tomriis@Toms-MacBook-Air.local>
2018-12-10 06:56:34 -0700

    manual focus working

--- FieldIIModels/concave_focused_array.m
+++ FieldIIModels/concave_focused_array.m
@@ -32,20 +32,20 @@ function [Th] = concave_focused_array(n_elements_x, ROC_x, P, D, R_focus, Nx, Ny
             for k=1:length(angle_y)
                 x = [-D(1)/2 D(1)/2]; y = [-(D(2)/Ny)/2 (D(2)/Ny)/2]; z = [0,0];
                 rect = [i x(1)  y(1)  z(1)  x(2)  y(1)  z(1)  x(2)  y(2)  z(2)  x(1)  y(2)  z(2)  1  D(1)  D(2)  0  0  0];
-                cent = rect(:, end-2:end);
-                Th = xdc_rectangles( rect, cent, [0,0,0]);
-                rect = xdc_pointer_to_rect(Th);
-                rect([4,7,10,13,19],:)=rect([4,7,10,13,19],:)+R_focus;
-                rot = makeyrotform(angle_y(k));
+                rect = rect';
+                rot = makexrotform(angle_y(k));
+                rect([4,7,10,13,19],:)=rect([4,7,10,13,19],:)+ROC_x;
                 positioned_rect = apply_affine_to_rect(rot, rect);
+    %           Append to transducer geometry
                 focused_rectangles = horzcat(focused_rectangles, positioned_rect);
             end
-            
             mv = max(focused_rectangles(end,:));
             focused_rectangles([4,7,10,13,19],:) = focused_rectangles([4,7,10,13,19],:) - mv;
+            % Place the static focus at the center of rotation
+            focus = [0, 0, -ROC_x];
+            % Convert to transducer pointer
             cent = focused_rectangles(end-2:end,:);
-            focused_rectangles(1,:) = i;
-            Th = xdc_rectangles( rect', cent', [0,0,-ROC_x]);
+            Th = xdc_rectangles(focused_rectangles', cent', focus);
         else
             % Flat
             x = [-D(1)/2 D(1)/2]; y = [-D(2)/2 D(2)/2]; z = [0,0];
@@ -56,8 +56,10 @@ function [Th] = concave_focused_array(n_elements_x, ROC_x, P, D, R_focus, Nx, Ny
         rect = xdc_pointer_to_rect(Th);
         rect(1,:) = i;
     % Flip tranducer 
+    if ~strcmp(type, 'focused2')
         rot = makeyrotform(pi);
         rect = apply_affine_to_rect(rot,rect);
+    end
     % Position transducer
         rot = makeyrotform(angle_x(i));
         rect([4,7,10,13,19],:)=rect([4,7,10,13,19],:)+ROC_x;
