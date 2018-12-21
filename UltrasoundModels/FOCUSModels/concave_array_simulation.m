7deea532e92509052cce653443599f17573c84b1
Tom Riis <tomriis@Toms-MacBook-Air.local>
2018-12-20 18:14:06 -0800

    ran FOCUS param search

--- UltrasoundModels/FOCUSModels/concave_array_simulation.m
+++ UltrasoundModels/FOCUSModels/concave_array_simulation.m
@@ -1,64 +1,69 @@
-function [txfield, xdc_data]=concave_array_simulation(varargin)
+function [p_cw, transducer_array]=concave_array_simulation(varargin)
 
 default_element_W = 1.5;
 expectedGeometries = {'focused','spherical','flat','focused2','linear'};
 
 p = inputParser;
-addRequired(p,'n_elements', @(x) isnumeric(x));
+addRequired(p,'n_elements_x', @(x) isnumeric(x));
+addRequired(p,'n_elements_y', @(x) isnumeric(x));
 addRequired(p,'ROC', @(x) isnumeric(x));
 addRequired(p,'D');
 addRequired(p, 'focal_point');
-addOptional(p, 'AngleOfExtent', 0, @(x) isnumeric(x));
-addOptional(p, 'P', default_element_W, @(x) isnumeric(x));
+addOptional(p, 'kerf',0.2);
+
 addOptional(p, 'element_geometry', 'flat', @(x) any(validatestring(x,expectedGeometries)));
 addOptional(p, 'R_focus', 1e4, @(x) isnumeric(x));
-addOptional(p, 'Nx', 4);
-addOptional(p, 'Ny', 4);
+
 addOptional(p,'visualize_transducer',false);
 addOptional(p,'visualize_output',true);
 addOptional(p,'Slice','xy');
 parse(p, varargin{:})
 
 % FNM CW CSA Example
-width = p.Results.D(1)*1e-3;
-height = p.Results.D(2)*1e-3;
-elements_x = p.Results.Nx;
-elements_y = p.Results.Ny;
-kerf = (p.Results.D(2)-p.Results.P)*1e-3;
+ROC = p.Results.ROC/1000; %mm
+n_elements_x = p.Results.n_elements_x;  %number of physical elements in X.
+n_elements_y = p.Results.n_elements_y;  %number of physical elements in Y.
+kerf = p.Results.kerf/1000;
+D = p.Results.D; %Diameter, width, and length of element (mm)
+width = D(1)/1000;
+height = D(2)/1000;
+element_geometry = p.Results.element_geometry;
+R_focus = p.Results.R_focus/1000;
 spacing = width + kerf;
-ROC = p.Results.ROC*1e-3;
 
-transducer_array = create_rect_csa(elements_x, elements_y, width, height, kerf, kerf, ROC);
-draw_array(transducer_array);
+transducer_array = create_rect_csa(n_elements_x, n_elements_y, width, height, kerf, kerf, ROC);
 
 define_media();
 f0 = 650e3;  %(Hz) center frequency of the transducer
-fs = f0 * 20*2;  %(Hz) sampling frequency of the simulation; 20 times the transducer frequency is enough
+fs = f0 * 20*1.5;  %(Hz) sampling frequency of the simulation; 20 times the transducer frequency is enough
 alpha = 0.5 * 100 / 1e6;  %(dB/m/Hz) attenuation of ultrasound in the brain
 lambda = (lossless.soundspeed / f0);
 focus = p.Results.focal_point*1e-3;
 focus_x = focus(1); focus_y = focus(2); focus_z = focus(3);
 
-ndiv = round(2 * width/lambda); 
 
 [x,y,z] = get_slice_xyz(p.Results.Slice, focus);
 coord_grid = xyz_to_coord_grid(x,y,z);
 
-
-transducer_array = find_single_focus_phase(transducer_array, focus_x, ...
-focus_y, focus_z, water, f0, 200);
+%transducer_array = set_time_delays(transducer_array, focus_x,focus_y, focus_z, water, fs);
+transducer_array = find_single_focus_phase(transducer_array, focus_x,focus_y, focus_z, water, f0, 200,false);
 % The next step is to run the FNM function and display the resulting
 % pressure field.
 ndiv=3;
-tic();
-disp('Calculating pressure field...');
+
+ndiv2 = round(2 * width/lambda); 
+
+if ndiv<ndiv2
+   ndiv = ndiv2;
+end
+ndiv=3;
 p_cw=cw_pressure(transducer_array, coord_grid, lossless, ndiv, f0);
-disp(['Simulation complete in ', num2str(toc()), ' seconds.'])
-figure();
-h = pcolor(x*1000,z*1000,rot90(squeeze(abs(p_cw)),3));
-set(h,'edgecolor','none');
-title('Pressure Field at y = 0 cm');
-xlabel('x (cm)');
-ylabel('z (cm)');
+
+%figure();
+%h = pcolor(x*1000,z*1000,rot90(squeeze(abs(p_cw)),3));
+%set(h,'edgecolor','none');
+%title('Pressure Field at y = 0 cm');
+%xlabel('x (cm)');
+%ylabel('z (cm)');
 
 end
--- UltrasoundModels/param_search.m
+++ UltrasoundModels/param_search.m
@@ -1,6 +1,6 @@
 function [data] = param_search()
 % Define parameters and ranges for simulation to search through
-outfile = './param_search_field.mat';
+outfile = './param_search_focus.mat';
 n_elements_x = [64];
 n_elements_y = [12,3,4];
 element_W_x = [1.0,2.0];%2.5,3.0,3.5,4];
@@ -8,9 +8,10 @@ element_W_y = [10,20];
 focus = [0,40];
 ROC = [120 160];
 Slice = {'xy','xz','yz'};
+
 total = length(element_W_x)*length(element_W_y)*length(focus)*(length(ROC)+1)*length(Slice);
 count = 0;
-
+M = 2;
 data = struct();
 for a = 1:length(n_elements_x)
     n_elements_x_i = n_elements_x(a);
@@ -33,20 +34,24 @@ for y = 1:length(element_W_y)
     H_i = element_W_y(y);
 for sl =1:length(Slice)
     slice_i=Slice{sl};
-R_focus = [ROC_i, 1e14];
-for kk = 1:2
-    R_focus_i =R_focus(kk);
+% R_focus = [ROC_i, 1e14];
+% for kk = 1:2
+%     R_focus_i =R_focus(kk);
 
     disp('-----------------------------------------')
     disp(strcat('      ',num2str(count),' of ', num2str(total)));
     count = count +1;
-    
-[txfeilddb, xdc_data] = human_array_simulation(n_elements_x_i, n_elements_y_i,ROC_i,...,
-[W_i H_i],[focus_i,0,-ROC_i],'element_geometry','focused2','R_focus',R_focus_i,...,
-'Slice',slice_i, 'visualize_output',false);
-
+if M == 1    
+    [txfeilddb, xdc_data] = human_array_simulation(n_elements_x_i, n_elements_y_i,ROC_i,...,
+    [W_i H_i],[focus_i,0,-ROC_i],'element_geometry','focused2','R_focus',R_focus_i,...,
+    'Slice',slice_i, 'visualize_output',false);
+else
+    R_focus_i = ROC;
+    [txfeilddb, xdc_data] = concave_array_simulation(n_elements_x_i, n_elements_y_i,ROC_i,...,
+    [W_i H_i],[focus_i,0,ROC_i],'element_geometry','focused2','Slice',slice_i);
+end
 s = struct();
-s.M = 1.0; s.NX = n_elements_x_i; s.NY =n_elements_y_i; s.ROC = ROC_i; s.W = W_i; s.H = H_i;
+s.M = 2.0; s.NX = n_elements_x_i; s.NY =n_elements_y_i; s.ROC = ROC_i; s.W = W_i; s.H = H_i;
 s.F = focus_i; s.Slice = slice_i; s.Ro = R_focus_i; 
 
 fname = fieldname_from_params(s);
@@ -55,7 +60,7 @@ data.(fname) = txfeilddb;
 % save the transducer geometry
 data.(strcat('G_',fname(1:end-8))) = xdc_data;
 
-end
+%end
 end
 end
 end
