e32ae9071eaf8a6fbf8323dcc008734e25ef6542
Tom Riis <tomriis@toms-air.uconnect.utah.edu>
2019-03-03 14:09:54 -0700

    set up 2D simulation

--- UltrasoundModels/KWaveModels/kwave_simulation.m
+++ UltrasoundModels/KWaveModels/kwave_simulation.m
@@ -1,32 +1,52 @@
-function [txfield,xdc_data] = kwave_simulation(varargin)
-% create the computational grid
-Nx = 256;
-Ny = 256;
-Nz = 256;
-dx = 1e-3;
-dy = 1e-3;
-dz = 1e-3;
-kgrid = makeGrid(Nx, dx, Ny, dy, Nz, dz);
-% number of grid points in the x (row) direction
-% number of grid points in the y (column) direction
-% grid point spacing in the x direction [m]
-% grid point spacing in the y direction [m]
-% define the medium properties
-medium.sound_speed = 1500*ones(Nx, Ny); % [m/s]
-medium.sound_speed(1:50, :) = 1800;     % [m/s]
-medium.density = 1040;                  % [kg/m^3]
-% define an initial pressure using makeDisc
-disc_x_pos = 75;
-disc_y_pos = 120;
-disc_radius = 8;
-disc_mag = 3;
-source.p0 = disc_mag*makeDisc(Nx, Ny, disc_x_pos, disc_y_pos, disc_radius);
-% define a Cartesian sensor mask of a centered circle with 50 sensor elements
-sensor_radius = 2.5e-3;                 % [m]
-num_sensor_points = 50;
-sensor.mask = makeCartCircle(sensor_radius, num_sensor_points);
-% run the simulation
-sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor,'DataCast', 'single');
-txfield = sensor_data;
-xdc_data = source;
+function [txfield,source,mask] = kwave_simulation(varargin)
+    
+    p = inputParser;
+    addRequired(p,'n_elements_r', @(x) isnumeric(x));
+    addRequired(p,'n_elements_y', @(x) isnumeric(x));
+    addRequired(p,'a', @(x) isnumeric(x));
+    addRequired(p,'b', @(x) isnumeric(x));
+    addRequired(p,'D');
+    addRequired(p, 'focal_point');
+    addOptional(p, 'Dim',2);
+    addOptional(p,'type','horizontal');
+    addOptional(p, 'kerf',0.4);
+    addOptional(p, 'R_focus', 1e4, @(x) isnumeric(x));
+    addOptional(p,'Slice','xy');
+    parse(p, varargin{:})
+    
+    Dimensions = p.Results.Dim;
+    % create the computational grid
+    Nx = 2048;
+    Ny = 64;
+    Nz = 128;
+    dx = 2e-4;
+    dy = 1e-3;
+    dz = 10e-4;
+    kgrid = makeGrid(Nx, dx, Ny, dy, Nz, dz);
+    % Define the source
+    a = p.Results.a; %mm
+    b = p.Results.b;
+    n_elements_r = p.Results.n_elements_r;  %number of physical elements in X.
+    n_elements_y = p.Results.n_elements_y;  %number of physical elements in Y.
+    kerf = p.Results.kerf;
+    D = p.Results.D; %Diameter, width, and length of element (mm)
+    R_focus = p.Results.R_focus;
+    type = p.Results.type;
+    
+    mask = kwave_focused_array(kgrid, n_elements_r, n_elements_y, kerf/1000, D/1000, R_focus/1000,a/1000,b/1000,type);
+ 
+    if Dimensions == 2
+        source.p_mask = reshape(any(mask,2),[kgrid.Nx,kgrid.Nz]);
+    end
+        
+    % define the medium properties
+    medium.sound_speed = 1500*ones(Nx, Ny, Nz); % [m/s]
+    medium.density = 1040;                  % [kg/m^3]
+    % define a sensor mask 
+
+    
+    % run the simulation
+    
+    %sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor,'DataCast', 'single');
+    txfield = 0;
 end
