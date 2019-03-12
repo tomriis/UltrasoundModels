2d87e00c42c43c8ca7de6e75fe245e3dbdaec446
unknown <tomriis11@gmail.com>
2019-03-12 16:33:22 -0600

    kgrid adjustments

--- UltrasoundModels/KWaveModels/define_kgrid.m
+++ UltrasoundModels/KWaveModels/define_kgrid.m
@@ -14,15 +14,8 @@ function kgrid = define_kgrid(rect,focus, kerf, fs,Dimensions, c, type)
     
     Nx = ceil(2*R(1)/dx)+padding;
     Ny = ceil(2*R(2)/dy)+padding;
+    Nz = ceil(2*R(3)/dz)+padding;
     
-    if strcmp(type, 'horizontal')
-        Nz = ceil(2*R(3)/dz)+padding;
-    else
-        if focus(3) < 0
-            R(3)= R(3)+abs(focus(3));
-        end
-        Nz = ceil(2*R(3)/dz)+padding;
-    end
     if Dimensions == 2
         kgrid = makeGrid(Nx, dx, Nz,dz);
     else
