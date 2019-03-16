e9ee1b0d50972272d0a99a478786dd11c106319d
Tom Riis <tomriis@Toms-MacBook-Air.local>
2019-03-15 21:50:16 -0600

    skull to mask

--- UltrasoundModels/KWaveModels/skull_to_mask.m
+++ UltrasoundModels/KWaveModels/skull_to_mask.m
@@ -5,15 +5,12 @@ function [mask,ijk_skull] = skull_to_mask(filename,kgrid, type)
    V = niftiread(filename);
    scale = zeros([1,3]);
    f = {'x_vec','y_vec','z_vec'};
-   %target = 
+
    for i = 1:3
-       %shift = kgrid.(f{i});
        rvec{i} = ((1:size(V,i))-size(V,i)/2)*info.PixelDimensions(i)/1000;
-       %rvec{i} = rvec{i} - shift(end-3);
-       
-       % Assumes voxels are equal dimension for kgrid
        scale(i) = floor((info.PixelDimensions(i)/(1000*kgrid.dx))/2);
    end
+  
    threshold = 1500; %1628
    V(1:88,:,:) = 0; % Left limit of skull side
    V(394:end,:,:) = 0; % Right limit of skull side
@@ -52,10 +49,16 @@ function [mask,ijk_skull] = skull_to_mask(filename,kgrid, type)
             ijk_skull = vertcat(ijk_skull, ij);
        end
        
+   else
+       
    end
    ijk_skull=unique(ijk_skull,'rows');
    for i = 1:length(ijk_skull)
-       mask(ijk_skull(i,1),ijk_skull(i,2))= 1;
+       if kgrid.dim == 2
+            mask(ijk_skull(i,1),ijk_skull(i,2))= 1;
+       else
+           mask(ijk_skull(i,1),ijk_skull(i,2),ijk_skull(i,3))=1;
+       end
    end
 
    
